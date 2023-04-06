28de945f552f279c946608ce969e1d0298b8e3ec
mahesh-kadam-patil <maheshkadampatil10@gmail.com>
2023-04-06 11:17:33 -0700

    file uploded

---
+++ src/main/java/com/example/demo/logic/Swaping.java
@@ -0,0 +1,25 @@
+package com.example.demo.logic;
+
+public class Swaping {
+
+	public static void main(String[] args) {
+		
+		
+		int a=10;
+		int b=20;
+		System.out.println("before swaping values "+a+ " "+b);
+		
+		a=a+b;//30
+		b=a-b;//20
+		a=a-b;
+		
+		System.out.println("after swaping values "+a+ " "+b );
+		
+	}
+	
+	
+	
+	
+	
+	
+}
